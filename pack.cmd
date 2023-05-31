68a5850c6081b97c724a7f3f642a818970d4146f
p1eXu5 <v1adp1exu5@gmail.com>
2023-05-31 17:02:01 +0300

    add pack script

---
+++ pack.cmd
@@ -0,0 +1,32 @@
+@echo off
+
+GOTO :PROGRAM
+
+:PROGRAM
+echo.
+echo Choice project:
+echo.
+echo     list                       List packages
+echo.
+echo     pack                       Pack `p1eXu5.FSharp.Ports`
+echo.
+echo.
+
+set /p "id=Enter ID: "
+
+IF        "%id%"=="list" (
+    dotnet fsi .\pack.fsx "list-packages"
+    GOTO :PROGRAM
+
+) ELSE IF "%id%"=="pack" (
+    dotnet fsi .\pack.fsx "pack-ports"
+    GOTO :PROGRAM
+
+) ELSE IF "%id%"=="e" (
+    echo exit
+    GOTO :EOF
+
+) ELSE (
+    echo unknown case
+    GOTO :PROGRAM
+)
